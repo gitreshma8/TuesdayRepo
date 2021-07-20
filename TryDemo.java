d97ebbf65178cea5bfa26a91217612f9ebdd70ff
Reshma <reshma.satheesh2010@gmail.com>
2021-07-20 16:25:01 +0530

    try commit

---
+++ TryDemo.java
@@ -0,0 +1,23 @@
+
+
+public class TryDemo {
+
+	public static void main(String[] args) {
+	
+	try{
+	int a=10,b=0,c;
+	c=a/b;
+	}catch(Exception e){
+		
+		System.out.println(e);
+		
+	System.out.println("cannot divide by zero" );	
+			
+	}
+	
+	System.out.println("not an abnormal termination" );	
+	
+	
+	}
+
+}
